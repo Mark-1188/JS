a465e7d4dbd2b85828d8b59c41768270663b27b0
Mark <vutukurimark.1188@gmail.com>
2023-10-06 08:47:10 +0530

    Commiting the Js

---
+++ new.js
@@ -0,0 +1,47 @@
+//Task-1//
+var a=prompt("Enter your Mobile Number")
+a==9704474272 ? console.log();("Valid Number"):console.log();("Invalid Number")
+
+//Task-2//
+var a=prompt("Enter the value of A");
+var b=prompt("Enter the value of B");
+a>b ? console.log("A is greater then B"):console.log();("B is greater than A")
+
+//Task-3//
+var a=+prompt("Enter the value of A")
+var b=+prompt("Enter the value of B")
+var c=+prompt("Enter the value of c")
+40==a+b+c ? console.log("Hello World"):console.log("It is not 40")
+
+//Task-4//
+var a=+prompt("Enter any value from '0' to '6'")
+
+a==0 ?
+console.log("Hey, Today is Monday"):
+a==1 ?
+console.log("Hey, Today is Tuesday"):
+a==2 ?
+console.log("Hey, Today is Wednesday"):
+a==3 ?
+console.log("Hey, Today is Thursday"):
+a==4 ?
+console.log("Hey, Today is Friday"):
+a==5 ?
+console.log("Hey, Today is Saturday"):
+a==6 ?
+console.log("Hey, Today is Sunday"):
+console.log("Invalid Number")
+
+//Task-5//
+a = 50
+var a = prompt("Enter the Value 50")
+a == 50 ? console.log(++a) : console.log(--a)
+
+//Task-6//
+var a=prompt("Enter the Name")
+a=="Mark" ? console.log("valid_Name"): console.log("Invalid_Name");
+
+//Task-7//
+var a=+prompt("Enter the value:")
+var b=+prompt("Enter the value:")
+a+b===50 ? console.log(a+b+50):console.log(a+b-50)
